13609e3f18e9e190120dc59c6077e37e4b7b4be1
Sennydo <sadhvikchelmas1312@gmail.com>
2025-11-24 21:11:46 -0800

    struct change

--- components/philos.tsx
+++ components/philos.tsx
@@ -1,29 +1,19 @@
 const SecondHead = () => {
 
     return (
-    <section id="home" className="pt-28 pb-16 px-4 relative overflow-hidden">
+    <section id="home" className="pt-28 pb-16 px-4 relative overflow-hidden ">
 
-      <div className="container mx-auto max-w-6xl relative z-10">
-        <div className="grid lg:grid-cols-2 gap-12 items-center">
+      <div className="container mx-auto max-w-6xl relative z-10 bg-[url('/bubble.svg')] bg-no-repeat bg-center bg-[length:200%_200%] bg-opacit">
+        <div className="items-center justify-center flex">
           <div className="space-y-6">
-            <p className="font-serif text-3xl lg:text-4xl text-primary italic">My Design Philosophy</p>
-            <p className="text-foreground/80 leading-relaxed max-w-lg text-justify">
+            <h1 className="font-serif text-7xl text-primary italic">My Design Philosophy</h1>
+            <h2 className="text-foreground/80 leading-relaxed max-w-lvw text-justify">
                 I see design as a way to express emotions through form, texture, and detail. 
                 My work explores the balance between structure and fluidity, allowing materials to tell stories 
                 inspired by people, culture, and everyday experiences. I believe design should go beyond aesthetics — 
                 it should evoke feeling, empower individuality, and create a sense of purpose. Each piece I create aims to 
                 connect meaningfully with the wearer and their world.
-            </p>
-          </div>
-
-          <div className="relative flex justify-center lg:justify-end">
-            <div className="image-fade-popup w-full max-w-md h-[500px]">
-              <img
-                src="/professional-woman-portrait.png"
-                alt="Kasturi Jana"
-                className="w-full h-full object-contain object-bottom"
-              />
-            </div>
+            </h2>
           </div>
         </div>
       </div>
